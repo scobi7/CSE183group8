72c01977daf9db867db25b5d1094a3c9cbf71530
josh lev <soybr3ad@gmail.com>
2024-12-09 17:59:05 -0800

    Adding checklist related actions to controllers

--- controllers.py
+++ controllers.py
@@ -30,6 +30,7 @@ from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from py4web.utils.url_signer import URLSigner
 from .models import get_user_email
+import json
 
 url_signer = URLSigner(session)
 
@@ -46,3 +47,123 @@ def index():
 def my_callback():
     # The return value should be a dictionary that will be sent as JSON.
     return dict(my_value=3)
+
+@action('checklist')
+@action.uses('checklist.html', db, auth.user, url_signer)
+def checklist():
+    if not auth.curren_user:
+        redirect(URL('auth/login'))
+    return dict(
+        checklist_data_url = URL('checklist_data'),
+        my_checklist_url = URL('my_checklist'),
+        update_sightings_url = URL('update_sightings')
+    )
+
+@action('checklist_data', method="GET")
+@action.uses(db,auth)
+def checklist_data():
+    species_sightings = db(db.sightings).select(
+        db.sightings.COMMON_NAME,
+        db.sightings.OBSERVATION_COUNT.sum(),
+        groupby=db.sightings.COMMON_NAME
+    ).as_list()
+    return dict(checklist_data=species_sightings)
+
+@action('my_checklists')
+@action.uses('my_checklists.html', db, auth.user, session)
+def my_checklists():
+    drawn_cordinates = session.get('drawn_coordinates', [])
+    return dict(
+        load_checklists_url=URL('load_checklists'),
+        delete_checklists_url=URL('delete_checklists'),
+        edit_checklists_url=URL('edit_checklists'),
+        drawn_cordinates = json.dumps(drawn_cordinates),
+    )
+
+@action('load_checklists')
+@action.uses(db,auth.user)
+def load_checklists():
+    user_email = get_user_email()
+    checklists = db(db.checklist.OBSERVER_ID == user_email).select().as_list()
+    return dict(checklists=checklists)
+
+@action('add_checklist')
+@action.uses('add_checklist.html', db, auth.user, url_signer, session)
+def add_checklist():
+    drawn_coordinates = session.get('drawn_coordinates')
+    if not auth.current_user:
+        redirect(URL('auth/login'))
+    return dict(
+        submit_checklist_url=URL('submit_checklist'),
+        drawn_coordinates = json.dumps(drawn_coordinates)
+    )
+
+@action('submit_checklist', method = 'POST')
+@action.uses(db, auth.user, url_signer)
+def submit_checklist():
+    if not auth.current_user:
+        redirect(URL('auth/login)'))
+    species_name = request.forms.get('species_name')
+    latitude = float(request.forms.get('latitude'))
+    longitude = float(request.forms.get('longitude'))
+    observation_date = request.form.get('observation_date')
+    time_observations_started = request.forms.get('time_observations_started')
+    duration_minutes = float(request.forms.get('duration_minutes'))
+
+    observer_id = get_user_email()
+
+    sighting = db(db.sightings.COMMON_NAME == species_name).select().first()
+    if not sighting:
+        return dict(message="Sighting for the given species not found")
+    
+    sampling_event_identifier = sighting.SAMPLING_EVENT_IDENTIFIER
+
+    db.checklist.inser(
+        SAMPLING_EVENT_IDENTIFIER=sampling_event_identifier,
+        LATITUDE=latitude,
+        LONGITUDE=longitude,
+        OBSERVATION_DATE=observation_date,
+        TIME_OBSERVATIONS_STARTED=time_observations_started,
+        OBSERVER_ID=observer_id,
+        DURATION_MINUTES=duration_minutes
+    )
+    
+    redirect(URL('my_checklists'))
+
+@action('delete_checklist/<checklist_id:int>', method='DELETE')
+@action.uses(db, auth.user, url_signer)
+def delete_checklist(checklist_id):
+    user_email = get_user_email()
+    checklist = db(db.checklist.id == checklist_id).select().first()
+
+    if not checklist:
+        return dict(message="Checklist not found")
+    if checklist.OBSERVER_ID != user_email:
+        return dict(message="User not authorized to delete checklist")
+    
+    db(db.checklist.id == checklist_id).delete()
+
+    return dict(message="Checklist Deleted")
+
+@action('edit_checklist/<checklist_id:int>', method=['GET','POST'])
+@action.uses(db,auth.user,url_signer)
+def edit_checklist(checklist_id):
+    checklist = db.checklist[checklist_id]
+    if not checklist:
+        redirect(URL('my_checklists'))
+    if request.method == 'GET':
+        return dict(
+            checklist=checklist,
+            checklist_id=checklist_id
+        )
+    if request.method == 'POST':
+        checklist.update_record(
+            SAMPLING_EVENT_IDENTIFIER = request.forms.get('sampling_event_identifier'),
+            LATITUDE=float(request.forms.get('latitude')),
+            LONGITUDE=float(request.forms.get('longitude')),
+            OBSERVATION_DATE=request.forms.get('observation_date'),
+            TIME_OBSERVATIONS_STARTED=request.gorms.get('time_observations_started'),
+            OBSERVER_ID=get_user_email(),
+            DURATION_MINUTES=float(request.forms.get('duration_minutes'))
+        )
+        redirect(URL('my_checklists'))
