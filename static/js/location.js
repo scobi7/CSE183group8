d7ad527ea9708d18636fb84ac08ee422a8fbe8f6
Jordan Lee <jlee528@ucsc.edu>
2024-12-10 17:56:39 -0800

    dropdown now reactive to current selected bird.

--- static/js/location.js
+++ static/js/location.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// This will be the object that will contain the Vue attributes
-// and be used to initialize it.
 let app = {};
 
 
@@ -9,23 +7,30 @@ app.data = {
     data: function() {
         return {
             chart: undefined,
-            chartData: [12, 19, 3, 5, 2, 3], //TODO change based on species selected
-            chartLabels: ['day 1?', 'day 2', 'day 3', 'day 4', 'day 5', 'day 6'], //change
+            chart_data: [12, 19, 3, 5, 2, 3], //TODO change based on species selected
+            chart_labels: ['day 1?', 'day 2', 'day 3', 'day 4', 'day 5', 'day 6'], //change
 
-            bird_list: [{species: "bluejay", count: 10}, {species: "sparrow", count: 20}],
-            dropdown_active: false,
-            topContributors: [{name: "guy", contributeCount: 5}, {name: "person", contributeCount: 2}],
+            bird_list: [{species: "bluejay", count: 10}, {species: "sparrow", count: 20}],  //not final structure
+            top_contributors: [{name: "guy", contributions: 5}, {name: "person", contributions: 2}], //not final structure
+
             total_sightings: 0,
+            total_checklists: 0,
 
+            //selected region coordinates
+            x1: 0,
+            y1: 0,
+
+            x2: 0,
+            y2: 0,
+
+            dropdown_active: false,
+            active_species: null,
         };
     },
-    methods: {
-        // Complete as you see fit.
-        my_function: function() {
-            // This is an example.
-            
-        },
-        
+    methods: {        
+
+        // TODO need the coordinates of the user's selected region
+
         // TODO need to fetch the list of Species in the Selected Region
         // TODO need to fetch the total number of sightings in Selected Region
 
@@ -46,10 +51,10 @@ app.data = {
             this.chart = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                labels: this.chartLabels,
+                labels: this.chart_labels,
                 datasets: [{
                     label: '# of Times Seen',  //TODO maybe change label can be a variable for species name + number of times seen
-                    data: this.chartData,
+                    data: this.chart_data,
                     borderWidth: 1
                     }]
                 },
@@ -76,14 +81,17 @@ app.data = {
 
         /********************
          * closes the dropdown menu for species selection and
-         * calls make_graph
+         * calls make_graph. Passes which bird is selected
          ********************/
-        select_bird: function(   ) {
+        select_bird: function(bird) {
             this.dropdown_active = false;
 
-            //TODO switch the selected bird and update graph
+            this.active_species = bird.species;
 
-            this.make_chart(); //need to pass in chartData and chartLabels but with bird specific data
+            //TODO switch the selected bird and update graph
+            //bird = {species: "name", count: number, }
+
+            this.make_chart(); //need to pass in chart_data and chart_labels but with bird specific data
         },
 
 
@@ -91,7 +99,7 @@ app.data = {
 
         //temp function
         test_button: function() {
-            this.chartData =  [1, 2, 3, 5, 2, 3];
+            this.chart_data =  [1, 2, 3, 5, 2, 3];
         },
 
         //temp function
@@ -109,7 +117,11 @@ app.load_data = function () {
     //axios.get(my_callback_url).then(function (r) {
     //    app.vue.my_value = r.data.my_value;
 
-    //load 
+    //TODO Load user selected coordinates
+
+    //TODO Load species
+
+    //TODO Load checklists
 
     //});
 
