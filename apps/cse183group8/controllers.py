95244e6ec39a89b72126178b309f81d288cb8530
Jordan Lee <jlee528@ucsc.edu>
2024-12-11 13:31:51 -0800

    location in controllers

--- apps/cse183group8/controllers.py
+++ apps/cse183group8/controllers.py
@@ -171,3 +171,49 @@ def edit_checklist(checklist_id):
             DURATION_MINUTES=float(request.forms.get('duration_minutes'))
         )
         redirect(URL('my_checklists'))
+
+
+@action('location')
+@action.uses('location.html', db, auth, url_signer)
+def location():
+    return dict(
+        get_location_data_url = URL('get_location_data'),
+    )
+
+@action('get_location_data')
+@action.uses(db, auth, url_signer)
+def get_location_data():
+
+    total_sightings = 0
+    total_checklists = 0
+    contributor_list = []
+    location_data = [] # [{'species': 'bird', 'day': ['2021-02-03'], 'count': [2]}]
+
+    checklist = db(db.checklist).select().as_list()
+    sightings = db(db.sightings).select().as_list()
+
+    for sight in sightings: 
+        checklist_data = db(db.checklist.SAMPLING_EVENT_IDENTIFIER == sight.SAMPLING_EVENT_IDENTIFIER).select().first() # all Sightings match to a single sampling event in Checklists
+        if not location_data: # if empty, populate with first item
+            location_data.append({
+                'species': sight.COMMON_NAME,
+                'day': [checklist_data.DATE], #list of dates
+                'count': [sight.OBSERVATION_COUNT] #list of counts, index aligned with the dates
+            })
+            #contributor_list.append({checklist_data.OBSERVER_ID})
+        else:
+            for check_location_data in location_data:
+                if check_location_data['species'] == sight.COMMON_NAME: # have seen the bird before
+                    # 
+                    if checklist_data.DATE in check_location_data['day']: 
+                        # add up count for this day
+                        get_index = check_location_data['day'].index(checklist_data.DATE) # index of the existing day
+                        check_location_data['count'][get_index] += sight.OBSERVATION_COUNT
+                        break
+                    else:
+                        # append day and count
+                        check_location_data['day'].append(checklist_data.DATE)
+                        check_location_data['count'].append(sight.OBSERVATION_COUNT)
+                        break
+
+    return dict(location_data=location_data)
